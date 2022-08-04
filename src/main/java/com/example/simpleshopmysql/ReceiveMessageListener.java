39be7b79c5652f08586153ba1e5bcea547de5130
Josh Ji <josh2013.880922@gmail.com>
2022-08-04 22:47:06 +0800

    Initial commit

---
+++ src/main/java/com/example/simpleshopmysql/ReceiveMessageListener.java
@@ -0,0 +1,32 @@
+package com.example.simpleshopmysql;
+
+import com.example.simpleshopmysql.models.Product;
+import com.example.simpleshopmysql.services.ProductService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReceiveMessageListener {
+
+	@Autowired
+	ProductService productService;
+
+	/**
+	 * 監聽Queue中是否有資料，若有資料則進行消費。
+	 * @param product
+	 */
+	@RabbitListener(queues={"tpu.queue"})
+	public Product receive(Product product) {
+		try {
+			Product newProduct = productService.addProduct(product);
+			return newProduct;
+		} catch (Exception e) {
+			Product newProduct = new Product();
+			newProduct.setName("null");
+			return newProduct;
+		}
+	}
+
+}
